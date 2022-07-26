d398e537228437ef6e44c77a08dd87e3e6f0c5c0
mauriciocoruja <mbjr@outlook.com.br>
2022-07-25 23:27:51 -0300

    doc: adicionando testes de documentação

--- simulador_financiamento/simulador_financiamento.py
+++ simulador_financiamento/simulador_financiamento.py
@@ -12,6 +12,17 @@ def calcular_juros(valor_do_financiamento: float,
     :param valor_do_financiamento: Valor financiado.
     :param taxa_de_juros: Valor da taxa de juros do financiamento.
     :return: Valor de juros referente ao valor do financiamento
+
+    **Examples**
+
+    >>> calcular_juros(100, 7.5)
+    0.625
+
+    >>> calcular_juros('',7.5)
+    Traceback (most recent call last):
+        ...
+    TypeError: can't multiply sequence by non-int of type 'float'
+
     """
     return valor_do_financiamento * ((taxa_de_juros / 100) / 12)
 
@@ -25,6 +36,14 @@ def calcular_parcela(valor_amortizacao: float,
     :param valor_juros: Valor de juros do financiamento — preferencialmente taxa de juros ao ano (ex.: 10% a.a)
     :param demais_valores: Valor de demais tarifas ou taxas (exemplo: Seguro, Taxa bancaria de financiamento)
     :return: Valor da parcela, baseado no valor restante do financiamento
+
+    **Examples**:
+
+    >>> calcular_parcela(200, 250)
+    450
+
+    >>> calcular_parcela(200, 250, 2000)
+    2450
     """
     return valor_amortizacao + valor_juros + demais_valores
 
@@ -35,6 +54,11 @@ def calcular_amortizacao(quantidade_parcelas: int, valor_emprestimo: float) -> f
     :param quantidade_parcelas: Quantidade de parcelas do financiamento
     :param valor_emprestimo: Valor financiado.
     :return: Valor a ser amortizado mensalmente
+
+    **Examples**
+
+    >>> calcular_amortizacao(360, 3600)
+    10.0
     """
     valor_amortizacao = valor_emprestimo / quantidade_parcelas
     return valor_amortizacao
@@ -50,6 +74,11 @@ def gerar_dados(valor_financiamento: float,
     :param quantidade_parcelas: Total de parcelas do financiamento
     :param amortizacao: Volor médio de amortizações mensais
     :return: Lista de tuplas com as informações geradas
+
+    **Examples**
+
+    >>> gerar_dados(1000,10,2)
+    [(508.3333333333333, 500.0, 0, 8.333333333333334, 500.0), (504.1666666666667, 500.0, 0.0, 4.166666666666667, 0.0)]
     """
     amortizacao_financiamento = calcular_amortizacao(quantidade_parcelas, valor_financiamento)
     dados = []
@@ -105,7 +134,13 @@ def calcular_totais(dados: list) -> tuple:
 
     :param dados: lista com os dados do financiamento calculados.
     :return: tupla com os dados totais calculados
-    """
+
+    **Examples**
+
+    >>> calcular_totais([(508.3333333333333, 500.0, 0, 8.333333333333334, 500.0),
+    ...        (504.1666666666667, 500.0, 0.0, 4.166666666666667, 0.0)])
+    (1012.5, 1000.0, 0.0, 12.5)
+"""
 
     valor_parcela, amortizacao_financiamento, amortizacao_adicional, juros = 0, 0, 0, 0
 
@@ -133,6 +168,13 @@ def adicionar_dados(valor_parcela: float,
     :param valor_financiamento:
     :param dados_financiamento:
     :return: none
+
+    **Examples**
+
+    >>> dados = [(508.3333333333333, 500.0, 0, 8.333333333333334, 500.0), (504.1666666666667, 500.0, 0.0, 4.166666666666667, 0.0)]
+    >>> adicionar_dados(510.10, 500.0, 0, 9.50, 500.0, dados)
+    >>> len(dados)
+    3
     """
     dados_financiamento.append(
         (valor_parcela,
@@ -149,6 +191,13 @@ def gerar_tabela_meses(dados_calculados: list,
     :param dados_calculados:
     :param start:
     :return: Conteudo do dataframe como string
+
+    **Examples**
+
+    >>> print(gerar_tabela_meses(list(f for f in gerar_dados(10000, 7.4, 2))))
+            Parcela Amortização Amortização adicional  Juros Saldo Devedor
+    07/22  5.061,67    5.000,00                  0,00  61,67      5.000,00
+    08/22  5.030,83    5.000,00                  0,00  30,83          0,00
     """
     dados_formatados = []
     for i in dados_calculados:
@@ -166,6 +215,13 @@ def gerar_tabela_parcela(dados_calculados: list) -> str:
 
     :param dados_calculados:
     :return: Conteudo do dataframe como string
+
+    **Examples**
+
+    >>> print(gerar_tabela_parcela(list(f for f in gerar_dados(10000, 7.4, 2))))
+        Parcela Amortização Amortização adicional  Juros Saldo Devedor
+    1  5.061,67    5.000,00                  0,00  61,67      5.000,00
+    2  5.030,83    5.000,00                  0,00  30,83          0,00
     """
 
     dados_formatados = []
@@ -191,6 +247,11 @@ def configurar_meses(dados: list,
     :param dados: dados do financiamento calculado.
     :param start: data de inicio do pagamento do financiamento, e estimar o fim do financiamento
     :return: lista de datas no formato m/a (07/22)
+
+    **Examples**
+
+    >>> configurar_meses(list(e for e in range(2)), '07/22')
+    ['07/22', '08/22']
     """
     start_strptime = datetime.strptime(start, "%m/%y")
     return [e.strftime("%m/%y") for e in
@@ -203,6 +264,10 @@ def formatar_valor(valor) -> str:
     :param valor: valor a ser formatado
     :return: string com o valor monetario formatado
 
+    **Examples**
+
+    >>> formatar_valor(1000.1245)
+    '1.000,12'
     """
     locale.setlocale(locale.LC_ALL, 'pt_BR')
     return locale.format_string("%.2f", valor, grouping=True)
